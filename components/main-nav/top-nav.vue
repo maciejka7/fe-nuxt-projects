d2a567f198562d98e14d904e2d93fd053d57cf75
Michał Maciejewski <maciejewskimichal7@gmail.com>
2024-12-11 15:21:04 +0000

    improve nav and add challange origin link

--- components/main-nav/top-nav.vue
+++ components/main-nav/top-nav.vue
@@ -5,6 +5,7 @@ interface ProjectsLink {
   name: string,
   to: string,
   level?: SkillLevel
+  feLink?: string
 }
 
 const { query } = useRoute()
@@ -14,19 +15,21 @@ const isNavVisible = computed(() => (!('nav' in query && query.nav === 'false'))
 const projects: ProjectsLink[] = [
 
   {
-    name: 'Skilled E-learning landing',
+    name: 'Skilled e-learning Landing Page',
     to: '/projects/skilled-elearning-landing',
-    level: 'junior'
+    level: 'junior',
+     feLink: 'https://www.frontendmentor.io/challenges/skilled-elearning-landing-page-S1ObDrZ8q'
   },
   {
-    name: 'Tech Book Club Landing',
+    name: 'Tech Book Club Landing Page',
     to: '/projects/techbook-club-landing',
-    level: 'junior'
+    level: 'junior',
+    feLink: 'https://www.frontendmentor.io/challenges/tech-book-club-landing-page-fZQidjHU73'
   }
 
 ]
 
-const levels: SkillLevel[] = ['newbie', 'junior', 'intermediate', 'advanced','guru']
+const levels: SkillLevel[] = ['newbie', 'junior', 'intermediate', 'advanced', 'guru']
 
 </script>
 
@@ -44,16 +47,26 @@ const levels: SkillLevel[] = ['newbie', 'junior', 'intermediate', 'advanced','gu
     <div v-if="isOpen" class="bg-rosePine-surface w-full absolute p-4 left-0 top-16 transition-all shadow-xl">
 
       <section>
-        <p class="px-2 pb-2 text-rosePine-muted text-sm"> Projects:</p>
-  
-        <NuxtLink v-for="project in projects" :to="project.to" @click="isOpen = false"
-          class=" text-rosePine-text border border-transparent px-2 w-full py-2 flex hover:border-rosePine-subtle transition-all ">
-          {{ project.name }}
-          <MainNavSkillBadge :level="project.level"/>
-        </NuxtLink>
+        <p class="px-2 pb-1 text-rosePine-muted text-sm"> Projects:</p>
+
+        <div v-for="project in projects" class="flex flex-row items-center w-full ">
+          <NuxtLink :to="project.to" @click="isOpen = false"
+            class="flex-row items-center gap-2 text-rosePine-text hover:text-rosePine-subtle transition-all px-2 py-2 flex  ">
+            {{ project.name }}
+            <MainNavSkillBadge :level="project.level" />
+          </NuxtLink>
+
+          <NuxtLink v-if="project.feLink" :to="project.feLink" target="_blank" class="flex m-0 p-0">
+            <span class="block border border-rosePine-muted rounded">
+              <Icon size="1rem" class="flex p-2" name="material-symbols-light:arrow-outward-rounded" />
+            </span>
+          </NuxtLink>
+        </div>
 
         <div class="flex flex-row text-xs text-rosePine-subtle px-2">
-          <p class="flex flex-row items-center mr-2" v-for="level in levels"> <MainNavSkillBadge :level="level"/>-{{ level }}</p>
+          <p class="flex flex-row items-center mr-2" v-for="level in levels">
+            <MainNavSkillBadge :level="level" />{{ level }}
+          </p>
         </div>
       </section>
     </div>
